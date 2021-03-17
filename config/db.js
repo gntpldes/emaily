0c71dc42abce55708ece9d7c8eb6b342506600cb
gntpldes <41797111+gntpldes@users.noreply.github.com>
2021-03-17 15:42:12 -0500

    Add files via upload

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoogse = require('mongoose'); 
+const config = require ('config'); 
+const db = config.get('mongoURI'); 
+
+const connectDB = async () => {
+    try {
+        await mongoogse.connect(db, {
+            useNewUrlParser: true
+        }); 
+
+        console.log('MongoDB connected....'); 
+    }catch(err){
+        console.error(err.message); 
+        //Exit process with failure
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB; 
---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports ={
+    googleClientID: '714228811904-0ugs0ooshv36ccomqf50554qmgvltvem.apps.googleusercontent.com', 
+    googleClientSecret: 'LlbQIjMqAIOOlpJmuV23HFop',
+    mongoURI: "mongodb+srv://testUser:testUser123@testapp.5zkbs.mongodb.net/myFirstDatabase", 
+    cookieKey: 'nlkjuhfadpuqbcpd9ucxjhpo!*#*^#@)Ylfgdajhdaibf'
+}
