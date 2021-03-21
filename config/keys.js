2cee4dcdabe221eae5cadc2e28443b5523ea421b
gntpldes <41797111+gntpldes@users.noreply.github.com>
2021-03-21 18:58:48 -0500

    Delete keys.js

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports ={
-    googleClientID: '714228811904-0ugs0ooshv36ccomqf50554qmgvltvem.apps.googleusercontent.com', 
-    googleClientSecret: 'LlbQIjMqAIOOlpJmuV23HFop',
-    mongoURI: "mongodb+srv://testUser:testUser123@testapp.5zkbs.mongodb.net/myFirstDatabase", 
-    cookieKey: 'nlkjuhfadpuqbcpd9ucxjhpo!*#*^#@)Ylfgdajhdaibf'
-}
