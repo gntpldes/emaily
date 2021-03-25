4afd6a7aa6e101f2542a6f26c103d86ea140bc6d
gntpldes <41797111+gntpldes@users.noreply.github.com>
2021-03-25 12:02:35 -0500

    Add files via upload

---
+++ client/src/components/Landing.js
@@ -0,0 +1,17 @@
+import React from 'react'; 
+
+const Landing = () => {
+    return (
+        <div style={{ textAlign: 'center'}}> 
+            <h1>
+                GPD Live Tests
+            </h1>
+            <br/>
+            <span>
+                Testing out your new products so you can hit the ground running. 
+            </span>
+        </div>
+    );
+};
+
+export default Landing; 
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,21 @@
+/*
+    CSS libraries
+    Note: Importing CSS 
+*/
+import 'materialize-css/dist/css/materialize.min.css'; 
+import "./index.css"; 
+//React Components
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux'; 
+import { createStore, applyMiddleware } from 'redux'; 
+import reduxThunk from 'redux-thunk'; 
+//App Components
+import App from './components/App';
+import reducers from './reducers'; 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//Exportable files
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.querySelector('#root'));
