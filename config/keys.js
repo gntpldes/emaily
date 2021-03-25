28d5038efde8ba319fe2695d4b980a7b9a314d04
gntpldes <41797111+gntpldes@users.noreply.github.com>
2021-03-25 12:01:15 -0500

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+module.exports ={
+    googleClientID: '714228811904-0ugs0ooshv36ccomqf50554qmgvltvem.apps.googleusercontent.com', 
+    googleClientSecret: 'LlbQIjMqAIOOlpJmuV23HFop',
+    mongoURI: "mongodb+srv://testUser:testUser123@testapp.5zkbs.mongodb.net/myFirstDatabase", 
+    cookieKey: 'nlkjuhfadpuqbcpd9ucxjhpo!*#*^#@)Ylfgdajhdaibf', 
+    stripePublishableKey: 'pk_test_51IY7l2CUPmVsU5UQxsF3Kl4MWLevlVEdMiuZHnruwdZvvzcC6DjyOlW04QZanIguuY5oiUqpXwC11PIR9GFH1gT700kTBgDLfp', 
+    stripeSecretKey: 'sk_test_51IY7l2CUPmVsU5UQa9Gp5c7KANr0cytt04xQl2Z5Y3uXpRp6IZmQPYSRyCOiUVwpa4mM20qm0lSQAACFQK1eYylj00Yo5vXuDs'
+}
--- index.js
+++ index.js
@@ -1,14 +1,19 @@
+//External Libraries/Variables
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session'); 
 const passport = require('passport'); 
+const bodyParser = require('body-parser'); 
 const keys = require('./config/keys');
+//Internal Requirements
 require('./models/User');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(express.json());
 
 app.use(
     cookieSession({
@@ -21,6 +26,16 @@ app.use(passport.initialize());
 app.use(passport.session()); 
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app); 
+
+if (process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build')); 
+
+    const path = require('path'); 
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')); 
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
